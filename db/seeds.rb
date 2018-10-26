d49e5242a262d966022506cb9c7522ca4bb1898b
alexandrafren <alexandra.fren@gmail.com>
2018-10-25 20:34:02 -0700

    initialize

---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+post1= Post.create(title: "Will Conor McGregor return to the UFC’s Octagon in 2018?", content: "After Conor McGregor’s blockbuster fight with Floyd Mayweather ended via 10th round TKO in Mayweather’s favor back in August, McGregor has been enjoying the fruits of his labor. Raking in a disclosed $100 million paycheck for the contest, Conor took some time off vacationing in Ibiza, snorkeling out at sea off his yacht, and even accompanied famous fashion icon Donatella Versace to the British Fashion Awards.
+
+McGregor has appeared absent from training as of late, but since the recent dawn of the new year, McGregor has posted a few pictures to his Instagram page posing with fellow SBG teammate Richie Smullen and head coach of SBG Dublin 24, retired ex UFC fighter, Paddy “The Hooligan” Holohan. McGregor’s posts indicate they were rounds spent rolling jiu-jitsu, something McGregor had to abandon in his Marquess of Queensberry ruled boxing match with Floyd.", published_date: "January 8, 2018", link: "https://thebodylockmma.com/ufc/will-conor-mcgregor-return-ufcs-octagon-2018/")
+
+
+
+post2 = Post.create(title: "Is it ‘do or die’ for Uriah Hall at UFC Fight Night 124?", content: "Uriah Hall is on the cusp of finally being a top 10 middleweight, and with a win this Sunday, he will most likely find himself there.
+
+Hall has a chance to put a very big name in Vitor Belfort on his resume in the co-main event at UFC Fight Night 124. The question is, which Uriah Hall will show up? Will it be the Hall who dropped jaws all around the world with his spinning wheelhouse kick KO of Adam Cella, or will it be the Hall who got stopped in the first round against Derek Brunson and Gegard Mousasi?
+
+Uriah Hall is as infamous for his inconsistency against top-tier opponents as he is for his KO of Cella during The Ultimate Fighter Season 17. Since that highlight-reel kick that put Uriah Hall on the map, it’s been an up and down career for the 33 year old from Jamaica.
+
+With his biggest win coming as a heavy underdog against Gegard Mousasi via impressive TKO back in September 2015, Hall has since lost to the likes of Robert Whitaker, Derek Brunson, and Gegard Mousasi in their latter scheduled rematch.", published_date: "January 13, 2018", link: "https://thebodylockmma.com/ufc/do-or-die-uriah-hall-ufc-fight-night-124/")
+
+
+post3 = Post.create(title: "Volkan Oezdemir: A chance of a lifetime", content: "Volkan “No Time” Oezdemir has a chance on Saturday night at UFC 220 that every MMA fighter dreams of getting: a life-changing opportunity to fight for a UFC championship belt.
+
+Coming off of the best performance of his career, a devastating knockout against Jimi Manuwa, Oezdemir needs to find a way to outdo himself going against one of the greatest light heavyweights in the world today, Daniel Cormier. This is a co-main event bout that features the always exciting and classic matchup in mixed martial arts, a lethal striker versus an exceptional wrestler.", published_date: "January 19, 2018", link: "https://thebodylockmma.com/ufc/volkan-oezdemir-chance-lifetime/")
+
+
